5e3c61a1afd829ce3ee08e346b87f095d720dc9a
Tim Adamczyk <tim@mstsolutions.com>
2020-02-16 15:29:26 -0700

    search feature

--- src/modules/my/sessionList/sessionList.js
+++ src/modules/my/sessionList/sessionList.js
@@ -1,17 +1,20 @@
 import { LightningElement, track } from 'lwc';
 import { getSessions } from 'data/sessionService';
 export default class SessionList extends LightningElement {
-  @track sessions = [];
-  connectedCallback() {
-    getSessions().then(result => {
-      this.sessions = this.allSessions = result;
-      window.console.log('here is your log hehe:', JSON.stringify(this.sessions));
-    });
-  }
-  handleSearchKeyInput(event) {
-    const searchKey = event.target.value.toLowerCase();
-    this.sessions = this.allSessions.filter(
-      session => session.name.toLowerCase().includes(searchKey)
-    );
-  }
+    @track sessions = [];
+    connectedCallback() {
+        getSessions().then(result => {
+            this.sessions = this.allSessions = result;
+            window.console.log(
+                'here is your log hehe:',
+                JSON.stringify(this.sessions)
+            );
+        });
+    }
+    handleSearchKeyInput(event) {
+        const searchKey = event.target.value.toLowerCase();
+        this.sessions = this.allSessions.filter(session =>
+            session.name.toLowerCase().includes(searchKey)
+        );
+    }
 }
