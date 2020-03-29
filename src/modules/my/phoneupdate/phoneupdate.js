1aaab14e1c84b9ab9ac5c3f8dbb6e29b6460508e
Tim Adamczyk <tim@mstsolutions.com>
2020-03-29 10:09:44 -0700

    pg

---
+++ src/modules/my/phoneupdate/phoneupdate.js
@@ -0,0 +1,4 @@
+import { LightningElement } from 'lwc';
+
+export default class phoneupdate extends LightningElement {
+}
--- src/modules/my/sessionList/sessionList.js
+++ src/modules/my/sessionList/sessionList.js
@@ -1,8 +1,11 @@
 import { LightningElement, track } from 'lwc';
 import { getSessions } from 'data/sessionService';
 export default class SessionList extends LightningElement {
+    
     @track sessions = [];
+   
     connectedCallback() {
+      
         getSessions().then(result => {
             this.sessions = this.allSessions = result;
             window.console.log(
@@ -11,10 +14,19 @@ export default class SessionList extends LightningElement {
             );
         });
     }
+
     handleSearchKeyInput(event) {
         const searchKey = event.target.value.toLowerCase();
         this.sessions = this.allSessions.filter(session =>
             session.name.toLowerCase().includes(searchKey)
         );
     }
+    
+    handleSessionClick(event) {
+        const index = event.currentTarget.dataset.index;
+        const navigateEvent = new CustomEvent('navigate', {
+          detail: this.sessions[index].id
+        });
+        this.dispatchEvent(navigateEvent);
+      }
 }
